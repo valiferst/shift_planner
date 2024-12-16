eefdb36d5a899da95fd4f5447bf4015c4f3e722e
Rasmus Hansen <rasmus.hansen@student.uibk.ac.at>
2024-12-16 16:56:58 +0100

    creates the Shift repo. Offers only bare-bone functionality inherited from the abstract repo (delete, findAll, findByID, save)

---
+++ src/main/java/at/qe/skeleton/repositories/ShiftRepository.java
@@ -0,0 +1,13 @@
+package at.qe.skeleton.repositories;
+
+import
+import at.qe.skeleton.model.Userx;. at.qe.skeleton.model.Shift;
+
+/**
+ * Repository for managing {@link Shift} entities.
+ *
+ */
+
+public interface ShiftRepository extends AbstractRepository<Shift, Long>{
+
+}
