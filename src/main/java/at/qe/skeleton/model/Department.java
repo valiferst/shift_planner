4e418e35bcdf4fb59a65dd686d51a08ebf0854a0
lucas <lucas.henze@student.uibk.ac.at>
2024-12-22 15:08:51 +0100

    Comments incorporated: Attributes updated, class implemented as serializable, comparable, persistable. Also implemented hashCode and toString. Deletion/cascading logic removed for now.

--- src/main/java/at/qe/skeleton/model/Department.java
+++ src/main/java/at/qe/skeleton/model/Department.java
@@ -1,44 +1,55 @@
 package at.qe.skeleton.model;
 
-
 import jakarta.persistence.*;
+import org.springframework.data.domain.Persistable;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-public class Department {
+public class Department implements Persistable<Long>, Serializable, Comparable<Department> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long departmentID;
-
     private String name;
-    private String city;
-    private int postCode;
-    private String street;
-    private int houseNumber;
+    private LocalDateTime openingTime;  // Öffnungszeit als LocalDateTime
+    private LocalDateTime closingTime;  // Schließzeit als LocalDateTime
 
-    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "department")
     private List<ShiftPlan> shiftPlans;
 
     @OneToOne
     @JoinColumn(name = "manager_id")
-    private User manager;
+    private Userx manager;
 
-    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<User> employees;
+    @OneToMany(mappedBy = "department")
+    private List<Userx> employees;
 
     // Constructors
-    public Department() {}
-
-    public Department(String name, String city, int postCode, String street, int houseNumber) {
+    // Constructor with basic attributes
+    public Department(String name, LocalDateTime openingTime, LocalDateTime closingTime) {
         this.name = name;
-        this.city = city;
-        this.postCode = postCode;
-        this.street = street;
-        this.houseNumber = houseNumber;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
     }
 
-    // Getters and Setters
+    // Extended constructor with all attributes
+    public Department(String name, LocalDateTime openingTime, LocalDateTime closingTime,
+                      List<ShiftPlan> shiftPlans, Userx manager, List<Userx> employees) {
+        this.name = name;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+        this.shiftPlans = shiftPlans;
+        this.manager = manager;
+        this.employees = employees;
+    }
+
+    public Department() {}
+
+    // Getters and Setter
     public Long getDepartmentID() {
         return departmentID;
     }
@@ -55,36 +66,20 @@ public class Department {
         this.name = name;
     }
 
-    public String getCity() {
-        return city;
+    public LocalDateTime getOpeningTime() {
+        return openingTime;
     }
 
-    public void setCity(String city) {
-        this.city = city;
+    public void setOpeningTime(LocalDateTime openingTime) {
+        this.openingTime = openingTime;
     }
 
-    public int getPostCode() {
-        return postCode;
+    public LocalDateTime getClosingTime() {
+        return closingTime;
     }
 
-    public void setPostCode(int postCode) {
-        this.postCode = postCode;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public int getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(int houseNumber) {
-        this.houseNumber = houseNumber;
+    public void setClosingTime(LocalDateTime closingTime) {
+        this.closingTime = closingTime;
     }
 
     public List<ShiftPlan> getShiftPlans() {
@@ -95,19 +90,58 @@ public class Department {
         this.shiftPlans = shiftPlans;
     }
 
-    public User getManager() {
+    public Userx getManager() {
         return manager;
     }
 
-    public void setManager(User manager) {
+    public void setManager(Userx manager) {
         this.manager = manager;
     }
 
-    public List<User> getEmployees() {
+    public List<Userx> getEmployees() {
         return employees;
     }
 
-    public void setEmployees(List<User> employees) {
+    public void setEmployees(List<Userx> employees) {
         this.employees = employees;
+    }
+
+    @Override
+    public Long getId() {
+        return this.departmentID;
+    }
+
+    @Override
+    public boolean isNew() {
+        return this.departmentID == null;
+    }
+
+    @Override
+    public int compareTo(Department other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.getId());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Department)) {
+            return false;
+        }
+        final Department other = (Department) obj;
+        return Objects.equals(this.getId(), other.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "at.qe.skeleton.model.Department[ id=" + departmentID + " ]";
     }
 }
