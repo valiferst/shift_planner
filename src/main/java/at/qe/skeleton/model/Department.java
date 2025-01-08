60e3c03277ae8693a088da732e2d9dc8589df733
valentinferst <valentin.ferst@student.uibk.ac.at>
2025-01-08 15:45:36 +0100

    updated the dummy classes Department.java and Shift.java

--- src/main/java/at/qe/skeleton/model/Department.java
+++ src/main/java/at/qe/skeleton/model/Department.java
@@ -1,4 +1,23 @@
+// Dummy class for Department
 package at.qe.skeleton.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/at/qe/skeleton/model/Shift.java
+++ src/main/java/at/qe/skeleton/model/Shift.java
@@ -1,4 +1,35 @@
+// Dummy class for Shift
 package at.qe.skeleton.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Shift {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private ShiftPlan shiftPlan;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ShiftPlan getShiftPlan() {
+        return shiftPlan;
+    }
+
+    public void setShiftPlan(ShiftPlan shiftPlan) {
+        this.shiftPlan = shiftPlan;
+    }
 }
