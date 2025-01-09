a0c23f1a6148f25bf1a9555cbac47c224ccf2734
Ronja Wackerle <Ronja.Wackerle@student.uibk.ac.at>
2025-01-09 11:31:31 +0100

    Added findByAttributes

--- src/main/java/at/qe/skeleton/repositories/ShiftRepository.java
+++ src/main/java/at/qe/skeleton/repositories/ShiftRepository.java
@@ -1,6 +1,13 @@
 package at.qe.skeleton.repositories;
 
 import at.qe.skeleton.model.Shift;
+import at.qe.skeleton.model.ShiftPlan;
+import at.qe.skeleton.model.Userx;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Repository for managing {@link Shift} entities.
@@ -9,4 +16,12 @@ import at.qe.skeleton.model.Shift;
 
 public interface ShiftRepository extends AbstractRepository<Shift, Long>{
 
+    Shift findFirstByStartTime(LocalDateTime startTime);
+
+    @Query("SELECT u FROM Shift u WHERE :worker MEMBER OF u.shiftWorkers")
+    List<Shift> findByShiftWorker(@Param("worker")Userx shiftWorker);
+
+    List<Shift> findByShiftPlan(ShiftPlan shiftPlan);
+
+    boolean existsByStartTime(LocalDateTime startTime);
 }
