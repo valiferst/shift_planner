60271a520d6c5350cd47c9e750ab23378d515ab3
Rasmus Hansen <rasmus.hansen@student.uibk.ac.at>
2025-01-09 12:24:14 +0100

    implements JSON serialization for the department typescript dto. Still have to consider manager name to manager id translation (see #100)

--- src/main/frontend/src/DTO/Department.ts
+++ src/main/frontend/src/DTO/Department.ts
@@ -10,9 +10,99 @@
 export interface DepartmentDTO {
     id?: number;
     name: string;
-    openingTime: Date;
-    closingTime: Date;
-    managerId: number;
+    openingTime: Date | null;
+    closingTime: Date | null;
+    managerId?: number| null; // TODO research optional values in ts
 }
 
-// TODO do we need the same JSON serialzitaiton capabilities as in the Userx.ts
+/**
+ * Department class with methods for serialization
+ */
+export class Department implements DepartmentDTO {
+
+    id?: number | undefined; //TODO what does undefined mean in this context? research optional values in ts/js
+    name: string;
+    openingTime: Date | null;
+    closingTime: Date | null;
+    managerId?: number| null;
+
+    /**
+     * Constructor for the Department class
+     * @param data :DepartmentDTO object
+     */
+    constructor(data: DepartmentDTO) {
+        this.id = data.id;
+        this.name = data.name;
+        this.openingTime = data.openingTime;
+        this.closingTime = data.closingTime;
+        this.managerId = data.managerId;
+    }
+
+    /**
+     * Serialize the Department instance to JSON
+     * @returns JSON object
+     */
+    toJSON(): DepartmentDTO {
+        return {
+            id: this.id,
+            name: this.name,
+            openingTime: this.openingTime,
+            closingTime: this.closingTime
+        };
+    }
+
+    // TODO implement Department creation via frontend with the manager as a manager name or user object and not just the id of the user object of the manager
+    /**
+     * Serialize the User instance to JSON for creating a new user
+     * @returns JSON object with the fields required for creating a new user
+     */
+    toCreateJSON(): Pick<DepartmentDTO, 'name' | 'openingTime' | 'closingTime' | 'managerId' > {
+        return {
+            name: this.name,
+            openingTime: this.openingTime,
+            closingTime: this.closingTime,
+            managerId: this.managerId
+        };
+    }
+
+    // TODO, again, how do we translate an actual manager (user object with name e.g. John Doe) to a managerId?
+    /**
+     * Serialize the User instance to JSON for updating an existing department
+     * @returns JSON object with the fields required for updating a department
+     */
+    toUpdateJSON(): Pick<DepartmentDTO, 'id'| 'name' | 'openingTime' | 'closingTime' | 'managerId'  > {
+        return {
+            id: this.id,
+            name: this.name,
+            openingTime: this.openingTime,
+            closingTime: this.closingTime,
+            managerId: this.managerId,
+        };
+    }
+
+    /**
+     * Create an empty Department instance
+     * @returns Department instance with empty fields
+     */
+    static empty(): Department {
+        return new Department({
+            id: undefined,
+            name: '',
+            openingTime: null,
+            closingTime: null,
+            managerId: null
+        });
+    }
+
+    /**
+     * Create a Department instance from a JSON object
+     * @param json
+     * @returns Department instance
+     */
+    static fromJSON(json: any): Department {
+        if (!json || typeof json !== 'object') {
+            throw new Error('Invalid JSON for Department');
+        }
+        return new Department(json);
+    }
+}
