30cadc774e554f37ed2cc77ac62f148854eac845
hannahveit <hannah.veit@student.uibk.ac.at>
2025-01-10 14:00:39 +0100

    created class AbsenceMapper

---
+++ src/main/java/at/qe/skeleton/mappers/AbsenceMapper.java
@@ -0,0 +1,12 @@
+package at.qe.skeleton.mappers;
+
+import at.qe.skeleton.dtos.UserxDTO;
+import at.qe.skeleton.model.Userx;
+import at.qe.skeleton.services.UserxService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AbsenceMapper implements DTOMapper<Absence, AbsenceDTO>{
+
+}
