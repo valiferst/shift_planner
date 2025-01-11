42f24c9c8e02d6da42251f8b002a34312592e07f
Ronja Wackerle <Ronja.Wackerle@student.uibk.ac.at>
2025-01-11 09:28:01 +0100

    added createShift to controller

--- src/main/java/at/qe/skeleton/controllers/ShiftController.java
+++ src/main/java/at/qe/skeleton/controllers/ShiftController.java
@@ -1,17 +1,20 @@
 package at.qe.skeleton.controllers;
 
 import at.qe.skeleton.dtos.ShiftDTO;
+import at.qe.skeleton.dtos.UserxCreateDTO;
 import at.qe.skeleton.dtos.UserxDTO;
 import at.qe.skeleton.mappers.ShiftMapper;
 import at.qe.skeleton.model.Shift;
 import at.qe.skeleton.model.Userx;
 import at.qe.skeleton.services.ShiftService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 /**
@@ -43,10 +46,24 @@ public class ShiftController {
     }
 
     /**
+     * Creates a shift if the shift is not yet there.
+     *
+     * @param shiftDto the shift tb created
+     * @return {@link ResponseEntity} with status {@code 201 (Created)} with the newly created shift in the body,
+     *          or with status {@code 409 (Conflict)} if the shift already exists
+     */
+    @PostMapping("")
+    public ResponseEntity<ShiftDTO> createShift(@Valid @RequestBody ShiftDTO shiftDto) {
+        Shift shift = shiftService.saveShift(shiftMapper.mapFrom(shiftDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(shiftMapper.mapTo(shift));
+    }
+
+    /**
      * Deletes shift of given id.
      *
      * @param id the id of the shift to delete
-     * @return {@link ResponseEntity} with status {@code 204 (No Content)} on successful delete, or with status {@code 404 (Not Found)} if no user with this id exists
+     * @return {@link ResponseEntity} with status {@code 204 (No Content)} on successful delete,
+     *          or with status {@code 404 (Not Found)} if no user with this id exists
      */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteShift(@PathVariable Long id) {
