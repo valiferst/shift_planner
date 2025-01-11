701d2268e1447edb898f8b4efe20bc8c1a1a0b09
Ronja Wackerle <Ronja.Wackerle@student.uibk.ac.at>
2025-01-11 08:54:51 +0100

    added getCurrentShift

--- src/main/java/at/qe/skeleton/controllers/ShiftController.java
+++ src/main/java/at/qe/skeleton/controllers/ShiftController.java
@@ -1,8 +1,20 @@
 package at.qe.skeleton.controllers;
 
+import at.qe.skeleton.dtos.ShiftDTO;
+import at.qe.skeleton.dtos.UserxDTO;
+import at.qe.skeleton.mappers.ShiftMapper;
+import at.qe.skeleton.model.Shift;
+import at.qe.skeleton.model.Userx;
+import at.qe.skeleton.services.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Optional;
 
 /**
  * Shift endpoints exposed by the server.
@@ -20,4 +32,17 @@ public class ShiftController {
         this.shiftMapper = shiftMapper;
         this.shiftService = shiftService;
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShiftDTO> getCurrentShift(@PathVariable Long id) {
+        Optional<Shift> currentShift = shiftService.loadShift(id);
+        if (currentShift.isPresent()) {
+            ShiftDTO shiftDto = shiftMapper.mapTo(currentShift.get());
+            return ResponseEntity.ok(shiftDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
