c56a645919fec547c654ce37e0eaaa2332d385d6
Rasmus Hansen <rasmus.hansen@student.uibk.ac.at>
2025-01-12 16:29:07 +0100

    merge-request !28 review: fix the sonarqube issues

--- src/main/java/at/qe/skeleton/services/AbsenceService.java
+++ src/main/java/at/qe/skeleton/services/AbsenceService.java
@@ -1,17 +1,12 @@
 package at.qe.skeleton.services;
-
-import at.qe.skeleton.exceptions.UsernameDuplicateException;
 import at.qe.skeleton.model.Absence;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import at.qe.skeleton.repositories.AbsenceRepository;
 import java.util.Optional;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Service for accessing and manipulating Absences.
@@ -47,9 +42,8 @@ public class AbsenceService {
      * @param absence the absence to delete
      */
     public void deleteAbsence(Absence absence) {
-        // :TODO: write some audit log stating who and when this user was permanently deleted.
         Optional<Absence> absenceOpt = absenceRepository.findById(absence.getId());
-        absenceOpt.ifPresent(absencex -> absenceRepository.delete(absencex));
+        absenceOpt.ifPresent(absenceRepository::delete);
     }
 
     /**
