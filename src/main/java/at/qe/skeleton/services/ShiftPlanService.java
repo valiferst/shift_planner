5e54e0cd6a60e406a9487c8fdccaf0386648f624
Leonhard Roman Pätz <leonhard.paetz@student.uibk.ac.at>
2025-01-13 02:29:05 +0100

    modify shift plan functions accessibility to manager, add todos for getting and validating ShiftPlans

--- src/main/java/at/qe/skeleton/services/ShiftPlanService.java
+++ src/main/java/at/qe/skeleton/services/ShiftPlanService.java
@@ -1,14 +1,11 @@
 package at.qe.skeleton.services;
 
+import at.qe.skeleton.model.Department;
 import at.qe.skeleton.model.ShiftPlan;
-import at.qe.skeleton.model.Userx;
 import at.qe.skeleton.repositories.ShiftPlanRepository;
-import at.qe.skeleton.repositories.UserxRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -22,12 +19,10 @@ import java.util.Optional;
 public class ShiftPlanService {
 
     private final ShiftPlanRepository shiftPlanRepository;
-    private final UserxRepository userRepository;
 
     @Autowired
-    public ShiftPlanService(ShiftPlanRepository shiftPlanRepository, UserxRepository userRepository) {
+    public ShiftPlanService(ShiftPlanRepository shiftPlanRepository) {
         this.shiftPlanRepository = shiftPlanRepository;
-        this.userRepository = userRepository;
     }
 
     /**
@@ -40,54 +35,44 @@ public class ShiftPlanService {
         return shiftPlanRepository.findAll();
     }
 
+    @PreAuthorize("hasAnyAuthority('MANAGER')")
+    public Collection<ShiftPlan> getAllShiftPlansForDepartment(Department department) {
+        return null; // TODO implement getting list of shift plans per department
+    }
+
     /**
      * Loads a single shift plan identified by its id.
      *
      * @param id the id to search for
      * @return the shift plan with the id
      */
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public Optional<ShiftPlan> loadShiftPlan(Long id) {
         return shiftPlanRepository.findById(id);
     }
 
     /**
-     * Saves the shift plan. This method will also set createDate for new entities
-     * or updateDate for updated entities. The user requesting this operation will
-     * also be stored as createUser or updateUser respectively.
+     * Saves the shift plan.
      *
      * @param shiftPlan the shift plan to save
      * @return the updated shift plan
      */
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public ShiftPlan saveShiftPlan(ShiftPlan shiftPlan) {
-        if (shiftPlan.isNew()) {
-            shiftPlan.setCreateUser(getAuthenticatedUser());
-        } else {
-            shiftPlan.setUpdateUser(getAuthenticatedUser());
-        }
         return shiftPlanRepository.save(shiftPlan);
     }
+
+    // TODO implement validate shiftplan (is this the right spot)?
 
     /**
      * Deletes the shift plan.
      *
      * @param shiftPlan the shift plan to delete
      */
-    @PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('MANAGER')")
     public void deleteShiftPlan(ShiftPlan shiftPlan) {
-        // :TODO: write some audit log stating who and when this shift plan was permanently deleted.
         Optional<ShiftPlan> shiftPlanOpt = shiftPlanRepository.findById(shiftPlan.getId());
         shiftPlanOpt.ifPresent(plan -> shiftPlanRepository.delete(plan));
     }
 
-    /**
-     * Returns the currently authenticated user.
-     *
-     * @return the authenticated user or null
-     */
-    public Userx getAuthenticatedUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return userRepository.findFirstByUsername(auth.getName()).orElse(null);
-    }
 }
