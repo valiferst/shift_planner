f4929b6c42c805540e3efa7469bb7b024520d6af
Andrea Mussmann <andrea.mussmann@uibk.ac.at>
2025-01-13 15:39:56 +0100

    possible fix for date and time input in absence dialogue exception

--- src/main/frontend/src/components/AbsenceDialog.tsx
+++ src/main/frontend/src/components/AbsenceDialog.tsx
@@ -3,11 +3,12 @@
  * Architecture" offered by Innsbruck University.
  */
 import React from 'react';
-import {Dialog} from 'primereact/dialog';
-import {Button} from "primereact/button";
-import {AbsenceDTO} from "../DTO/Absence";
+import { Dialog } from 'primereact/dialog';
+import { Button } from "primereact/button";
+import { AbsenceDTO } from "../DTO/Absence";
 import AbsenceForm from './AbsenceForm';
-import {InputMaskChangeEvent} from "primereact/inputmask";
+import { InputMaskChangeEvent } from "primereact/inputmask";
+import { Nullable } from 'primereact/ts-helpers';
 
 interface AbsenceDialogProps {
     visible: boolean,
@@ -16,6 +17,8 @@ interface AbsenceDialogProps {
     onHide: () => void,
     onSubmit: () => void,
     onInputChange: (event: React.ChangeEvent<HTMLInputElement> | InputMaskChangeEvent) => void,
+    //onTimeChange: (name: 'absentFrom' | 'absentUntil', event: FormEvent<(Date | null)[], SyntheticEvent<Element, Event>>) => void,
+    onTimeChange: (name: 'absentFrom' | 'absentUntil' | 'validFrom' | 'validUntil', event: Nullable<Date>) => void,
 }
 
 /**
@@ -28,22 +31,23 @@ interface AbsenceDialogProps {
  * @param onInputChange callback when the input changes
  */
 const AbsenceDialog: React.FC<AbsenceDialogProps> = ({
-                                                         visible,
-                                                         absence,
-                                                         isNewAbsence,
-                                                         onHide,
-                                                         onSubmit,
-                                                         onInputChange,
-                                                     }) => {
+    visible,
+    absence,
+    isNewAbsence,
+    onHide,
+    onSubmit,
+    onInputChange,
+    onTimeChange,
+}) => {
 
     /**
      * Renders the footer of the dialog.
      */
     const renderFooter = () => (
         <div>
-            <Button label="Cancel" icon="pi pi-times" onClick={onHide} className="p-button-text"/>
+            <Button label="Cancel" icon="pi pi-times" onClick={onHide} className="p-button-text" />
             <Button label={isNewAbsence ? "Create" : "Save"} icon="pi pi-check" onClick={onSubmit}
-                    autoFocus/>
+                autoFocus />
         </div>
     );
 
@@ -51,7 +55,7 @@ const AbsenceDialog: React.FC<AbsenceDialogProps> = ({
         <Dialog
             header={isNewAbsence ? "Create New Absence" : "Edit Absence"}
             visible={visible}
-            style={{width: '50vw'}}
+            style={{ width: '50vw' }}
             onHide={onHide}
             footer={renderFooter}
         >
@@ -60,6 +64,7 @@ const AbsenceDialog: React.FC<AbsenceDialogProps> = ({
                     absence={absence}
                     isNewAbsence={isNewAbsence}
                     onInputChange={onInputChange}
+                    onTimeChange={onTimeChange}
                 />
             )}
         </Dialog>
