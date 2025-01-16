b26c681406655c431285c61451dc1e008d847df1
Ronja Wackerle <Ronja.Wackerle@student.uibk.ac.at>
2025-01-16 10:09:54 +0100

    cleanup shiftService delete

--- src/main/java/at/qe/skeleton/services/ShiftService.java
+++ src/main/java/at/qe/skeleton/services/ShiftService.java
@@ -87,9 +87,7 @@ public class ShiftService {
      */
     @PreAuthorize("hasAuthority('MANAGER')")
     public void deleteShift(Shift shift) {
-        // :TODO: write some audit log stating who and when this user was permanently deleted.
-        Optional<Shift> shiftOpt = shiftRepository.findById(shift.getId());
-        shiftOpt.ifPresent(shiftx -> shiftRepository.delete(shiftx));
+        shiftRepository.findById(shift.getId()).ifPresent(shiftRepository::delete);
     }
 
     /**
